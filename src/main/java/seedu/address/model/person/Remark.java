b77d5aa347927cc3a866fb01bb7325eaea6ef2d5
NigelGM <nigelgmorais@gmail.com>
2026-02-28 23:25:20 +0800

    Complete Add Remark tutorial

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -2,9 +2,17 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Represents a Person's remark in the address book.
+ */
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
