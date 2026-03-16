e88d47af23c261e3fba7a45a8dac99a807ca43ec
annamacinnes <anna.macinnes05@gmail.com>
2026-03-17 00:21:08 +0800

    Update Exit feature with UI changes and test updates

--- src/main/java/seedu/address/logic/parser/ExitCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ExitCommandParser.java
@@ -20,7 +20,7 @@ public class ExitCommandParser implements Parser<ExitCommand> {
      */
     @Override
     public ExitCommand parse(String arguments) throws ParseException {
-        if (!arguments.trim().isEmpty()){
+        if (!arguments.trim().isEmpty()) {
             throw new ParseException(ExitCommand.MESSAGE_EXTRA_PARAMETERS);
         }
         return new ExitCommand();
--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -16,6 +17,9 @@ public class ResultDisplay extends UiPart<Region> {
     @FXML
     private TextArea resultDisplay;
 
+    @FXML
+    private StackPane placeHolder;
+
     public ResultDisplay() {
         super(FXML);
     }
@@ -23,6 +27,19 @@ public class ResultDisplay extends UiPart<Region> {
     public void setFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
         resultDisplay.setText(feedbackToUser);
+        placeHolder.getStyleClass().remove("exit-message");
     }
 
+    /**
+     * Displays the exit message in a green box.
+     * This is specific to the ExitCommand.
+     */
+    public void setExitMessage(String message) {
+        requireNonNull(message);
+        resultDisplay.setText(message);
+
+        if (!placeHolder.getStyleClass().contains("exit-message")) {
+            placeHolder.getStyleClass().add("exit-message");
+        }
+    }
 }
