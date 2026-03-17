4943cd7014047422da69d71c1e60c265fb67b24d
e1384467 <e1384467@u.nus.edu>
2026-03-18 01:49:43 +0800

    Refactor Tag model to Symptom and update related references

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,7 +39,7 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
-    private FlowPane tags;
+    private FlowPane symptoms;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -52,8 +52,8 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        person.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        person.getSymptoms().stream()
+                .sorted(Comparator.comparing(symptom -> symptom.symptomName))
+                .forEach(symptom -> symptoms.getChildren().add(new Label(symptom.symptomName)));
     }
 }
